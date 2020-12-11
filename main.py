527ef1d209f76973a184e894b09cbf123a79741f
Jamison Engels <engelsj@tcd.ie>
2020-12-11 20:44:28 +0000

    Cleaned data

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.compose import ColumnTransformer
 
 
 def load_data(filename):
@@ -16,31 +18,21 @@ def load_data(filename):
     return df
 
 
-def separate_features(data_set):
-    data = data_set.values
-    X = data[:, 1:-1].astype(str)
-    y = data[:, 0].astype(str)
-    return X, y
-
-
 def main():
-    data_set = load_data("car-accident-data.csv")
-    X, y = separate_features(data_set)
-    ordinal_encoder = OrdinalEncoder()
-    X = ordinal_encoder.fit_transform(X)
-    label_encoder = LabelEncoder()
-    y = label_encoder.fit_transform(y)
-    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.33, random_state=1)
+    pd.set_option('display.max_columns', None)
+    df = load_data("car-accident-data.csv")
+    output_data = df["Accident severity"]
+    input_data = df.drop(["Accident severity"], axis=1)
 
-    ordinal_encoder = OrdinalEncoder()
-    ordinal_encoder.fit(Xtrain)
-    Xtrain = ordinal_encoder.transform(Xtrain)
-    Xtest = ordinal_encoder.transform(Xtest)
+    pipeline = ColumnTransformer([
+        ("cat", OneHotEncoder(), ["Region", "Light condition", "Weather condition", "Road surface"]),
+        ("ord", OrdinalEncoder(), ["Speed limit"])
+    ])
 
-    label_encoder = LabelEncoder()
-    label_encoder.fit(ytrain)
-    ytrain = label_encoder.transform(ytrain)
-    ytest = label_encoder.transform(ytest)
+    input_prepared = pipeline.fit_transform(input_data)
+    output_prepared = LabelEncoder().fit_transform(output_data)
+
+    Xtrain, Xtest, ytrain, ytest = train_test_split(input_prepared, output_prepared, test_size=0.33, random_state=1)
 
     model = LogisticRegression()
     model.fit(Xtrain, ytrain)

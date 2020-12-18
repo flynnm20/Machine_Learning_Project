24a20a9cb1e34dcac060b0ea915f9d34abc35983
Jamison Engels <engelsj@tcd.ie>
2020-12-18 17:30:01 +0000

    Added kNN back

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import chi2_contingency
 from sklearn.compose import ColumnTransformer
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from tensorflow.keras import regularizers
-from matplotlib import pyplot
+from sklearn.metrics import classification_report
 from keras.utils import to_categorical
 from sklearn.utils import resample
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
 
 
 def load_data(filename):
@@ -38,6 +41,7 @@ def downSample(df):
     df_downsampled = pd.concat([df_slight_down_sampled, df_serious_down_sampled, df_minority])
     return df_downsampled
 
+
 def logistic_regression(input_data, output_data):
     pipeline = ColumnTransformer([
         ("cat", OneHotEncoder(), ["Region", "Light condition", "Weather condition", "Road surface"]),
@@ -51,8 +55,20 @@ def logistic_regression(input_data, output_data):
     model = LogisticRegression()
     model.fit(Xtrain, ytrain)
     ypred = model.predict(Xtest)
-    accuracy = accuracy_score(ytest, ypred)
-    print('Logistic Regression --> Accuracy: %.2f' % (accuracy * 100))
+    from sklearn.metrics import f1_score
+    f1_score = f1_score(ytest, ypred, average='macro')
+    print(classification_report(ytest, ypred))
+    print('Logistic Regression --> f-1 score: %.2f' % (f1_score * 100))
+
+
+def baseline_model():
+    # create model
+    model = Sequential()
+    model.add(Dense(8, input_dim=33, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
 
 def neural_net(input_data, output_data):
@@ -64,44 +80,40 @@ def neural_net(input_data, output_data):
     input_prepared = pipeline.fit_transform(input_data)
     output_prepared = to_categorical(LabelEncoder().fit_transform(output_data))
 
-    Xtrain, Xtest, ytrain, ytest = train_test_split(input_prepared, output_prepared, test_size=0.33, random_state=1)
+    estimator = KerasClassifier(build_fn=baseline_model, epochs=20, batch_size=5, verbose=0)
+    kfold = KFold(n_splits=10, shuffle=True)
+    results = cross_val_score(estimator,  input_prepared, output_prepared, cv=kfold)
+    print("Baseline: " + (results.mean() * 100, results.std() * 100))
 
-    from keras.models import Sequential
-    from keras.layers import Dense
-    # define the keras model
-    model = Sequential()
-    model.add(Dense(20, input_dim=33, activation='relu'))
-    model.add(Dense(3, activation='softmax'))
-    model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
-    # fit model
-    history = model.fit(Xtrain, ytrain, validation_data=(Xtest, ytest), epochs=20, verbose=0)
-    # evaluate the model
-    _, train_acc = model.evaluate(Xtrain, ytrain, verbose=0)
-    _, test_acc = model.evaluate(Xtest, ytest, verbose=0)
-    print('NN --> Train: %.3f, Test: %.3f' % (train_acc, test_acc))
-    # plot loss during training
-    pyplot.subplot(211)
-    pyplot.title('Loss')
-    pyplot.plot(history.history['loss'], label='train')
-    pyplot.plot(history.history['val_loss'], label='test')
-    pyplot.legend()
-    # plot accuracy during training
-    pyplot.subplot(212)
-    pyplot.title('Accuracy')
-    pyplot.plot(history.history['accuracy'], label='train')
-    pyplot.plot(history.history['val_accuracy'], label='test')
-    pyplot.legend()
-    pyplot.show()
+
+def kNN(input_data, output_data):
+    pipeline = ColumnTransformer([
+        ("cat", OneHotEncoder(), ["Region", "Light condition", "Weather condition", "Road surface"]),
+        ("ord", OrdinalEncoder(), ["Speed limit"])
+    ])
+
+    input_prepared = pipeline.fit_transform(input_data)
+    output_prepared = to_categorical(LabelEncoder().fit_transform(output_data))
+
+    Xtrain, Xtest, ytrain, ytest = train_test_split(input_prepared, output_prepared, test_size=0.33, random_state=1)
+    neigh = KNeighborsClassifier(n_neighbors=20)
+    neigh.fit(input_prepared, output_prepared)
+    ypred = neigh.predict(Xtest)
+    from sklearn.metrics import f1_score
+    f1_score = f1_score(ytest, ypred, average='macro')
+    print(f1_score)
 
 
 def main():
     pd.set_option('display.max_columns', None)
     df = load_data("car-accident-data.csv")
+    print(df['Accident severity'].value_counts(sort=True))
     df = downSample(df)
     output_data = df["Accident severity"]
     input_data = df.drop(["Accident severity"], axis=1)
 
-    logistic_regression(input_data, output_data)
+    #logistic_regression(input_data, output_data)
+
     neural_net(input_data, output_data)
 
 

5ae7a71f58a6a03cf9b1b67662a36c85d32d9ffc
flynnm20 <46966461+flynnm20@users.noreply.github.com>
2020-12-20 17:22:46 +0000

    Cleaned code.

--- kNN_classifier.py
+++ kNN_classifier.py
@@ -22,14 +22,14 @@ def test_neighbors(input_prepared, output_prepared):
     mean_errs = []
     std_errs = []
     for i in kfolds:
-        gamma = i
         kf = KFold(n_splits=i)
         tmpMeanErr = []
         for trainer, tester in kf.split(input_prepared):
             model = KNeighborsClassifier(n_neighbors=i, weights='uniform').fit(input_prepared[trainer],
                                                                                output_prepared[trainer])
             ypred = model.predict(input_prepared[tester])
-            predError = f1_score(output_prepared[tester], ypred, average='weighted')
+            predError = f1_score(
+                output_prepared[tester], ypred, average='weighted')
             tmpMeanErr.append(predError)
         mean_errs.append(np.mean(tmpMeanErr))
         std_errs.append(np.std(tmpMeanErr))
@@ -55,7 +55,8 @@ def test_gamma(input_prepared, output_prepared):
             model = KNeighborsClassifier(
                 n_neighbors=k, weights=gaussian_kernel).fit(input_prepared[trainer], output_prepared[trainer])
             ypred = model.predict(input_prepared[tester])
-            predError = f1_score(output_prepared[tester], ypred, average='weighted')
+            predError = f1_score(
+                output_prepared[tester], ypred, average='weighted')
             tmpMeanErr.append(predError)
         mean_errs.append(np.mean(tmpMeanErr))
         std_errs.append(np.std(tmpMeanErr))
@@ -66,33 +67,36 @@ def test_gamma(input_prepared, output_prepared):
     plt.savefig("Graphs/kNN_Gamma_Crossval")
     plt.close()
 
-def compare_tuned_model(input_data, output_data):
-    ## Tunned Model
-    Xtrain, Xtest, ytrain, ytest = train_test_split(input_data, output_data, test_size=0.33, random_state=1)
-    tunned_knn_model = KNeighborsClassifier(n_neighbors=100, weights='uniform').fit(Xtrain, ytrain)
-    tuned_model_ypred = tunned_knn_model.predict(Xtest)
-    print("Tuned Knn f-1 score: " + str(f1_score(ytest, tuned_model_ypred, average='weighted')))
 
-    ## Dummy Model
+def compare_tuned_model(input_data, output_data):
+    # Tunned Model
+    Xtrain, Xtest, ytrain, ytest = train_test_split(
+        input_data, output_data, test_size=0.33, random_state=1)
+    tunned_knn_model = KNeighborsClassifier(
+        n_neighbors=50, weights='uniform').fit(Xtrain, ytrain)
+    tuned_model_ypred = tunned_knn_model.predict(Xtest)
+    print("Tuned Knn f-1 score: " +
+          str(f1_score(ytest, tuned_model_ypred, average='weighted')))
+
+    # Dummy Model
     dummy_classifier = DummyClassifier(strategy="most_frequent")
     dummy_classifier.fit(Xtrain, ytrain)
     dummy_classifier_ypred = dummy_classifier.predict(Xtest)
-    print("Dummy Model f-1 score: " + str(f1_score(ytest, dummy_classifier_ypred, average='weighted')))
+    print("Dummy Model f-1 score: " +
+          str(f1_score(ytest, dummy_classifier_ypred, average='weighted')))
 
     pred_prob = tunned_knn_model.predict_proba(Xtest)
     plot_multi_ROC(pred_prob, ytest, 'kNN')
 
-# Distance metric = Euclidean
-# k = [5 or 10] need to test.
-# Weight = Gaussian/uniform need to test gamma values
-# aggregation = Classifiaction
-# need to test C as well for alpha
+# does all experiments and tests tuned model against baseline model.
 def knn_classification(input_data, output_data):
     # gamma test
-    # gamma doesn't effect the overall accuracy.
     test_gamma(input_data, output_data)
+    # resulting graph proves that gamma is irrelevant for this set
 
     # k test
     test_neighbors(input_data, output_data)
+    # optimal k value is 50. Rather high and probably contributes to overfitting but smaller values are far less accurate
     # Increasing K tends to increase the overall accuracy because the data is so big.
+    # use tuned model K = 50 and Weight is uniform.
     compare_tuned_model(input_data, output_data)
